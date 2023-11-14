2c0846ecc5ceabf598a4884aaa9311b392725c20
luissilvestre04 <tvsilvestree@gmail.com>
2023-11-14 19:31:38 -0300

    commit de acerto

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ var chute = prompt('Chute um número de 1 a 100')
 
 //se o chute for igual ao numero mentalizado 
   if (chute == numeroMentalizado) {
-  alert('Certa resposta')  
-  } 
+  alert('Parabens você acertou!')  
+  } 43 
+  
+  
+  
