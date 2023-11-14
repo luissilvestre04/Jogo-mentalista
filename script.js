e02719e4a1827d278c955c5d47aa42e086fbf4c0
luissilvestre04 <tvsilvestree@gmail.com>
2023-11-14 19:24:50 -0300

    add javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+var numeroMentalizado = 43
+
+var chute = prompt('Chute um número de 1 a 100')
+
+//se o chute for igual ao numero mentalizado 
+  if (chute == numeroMentalizado) {
+  alert('Certa resposta')  
+  } 
