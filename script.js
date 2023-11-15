bfb38ce9b80adead96eeb3f91b7ff8b9e0c42d53
luissilvestre04 <tvsilvestree@gmail.com>
2023-11-15 03:05:38 -0300

    ramdom number commit

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
- //quando o chute for diferente
- while (chute!=numeroMentalizado) {
-      
- 
-  var numeroMentalizado = 43
+//número ramdômico
 
-  var chute = prompt('Chute um número de 1 a 100')
+var numeroMentalizado = parseInt(Math.random() * 101)
 
-//se o chute for igual ao numero mentalizado 
+while (chute != numeroMentalizado) {
+  var chute = prompt("Chute um número de 0 a 100")
+  //se o chute for igual ao numero mentalizado
   if (chute == numeroMentalizado) {
-  alert('Parabens você acertou!')  
-  //se o chute for menor que o numero
-  } else if (chute > numeroMentalizado){
-    alert('Errou... o número é menor tente novamente')
+    alert("Parabens você acertou!")
+    //se o chute for menor que o numero
+  } else if (chute > numeroMentalizado) {
+    alert("Errou... o número é menor tente novamente")
   }
   //se o chute for maior
   else if (chute < numeroMentalizado) {
-    alert('Errou o número é maior tente novamente')  
+    alert("Errou... o número é maior tente novamente")
   }
 }
