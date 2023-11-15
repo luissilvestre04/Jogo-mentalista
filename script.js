cdf904e79cf13f95c878b8f025cf49be4e4ba0d3
luissilvestre04 <tvsilvestree@gmail.com>
2023-11-15 01:04:21 -0300

    se a advinhaçao for maior ou menor

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ var chute = prompt('Chute um número de 1 a 100')
 //se o chute for igual ao numero mentalizado 
   if (chute == numeroMentalizado) {
   alert('Parabens você acertou!')  
-  } 43 
-  
-  
-  
+  //se o chute for menor que o numero
+  } else if (chute > numeroMentalizado){
+    alert('Errou... o número é menor tente novamente')
+  }
+  //se o chute for maior
+  else if (chute < numeroMentalizado) {
+    alert('Errou o número é maior tente novamente')  
+  }
